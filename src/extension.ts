763e74aceb661e4e0234d8e4006cd07b995fef58
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-11-01 00:10:06 +0900

    final commit :)

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let runrun = vscode.commands.registerCommand('run-something.runrun', () => {
+		//get path of current file
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) {
+			vscode.window.showInformationMessage('No active editor');
+			return;
+		}
+		const currentPath = activeEditor.document.fileName;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "run-something" is now active!');
+		const script = vscode.workspace
+			.getConfiguration('run-something')
+			.get('script') as string;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('run-something.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from run-something!');
+		// run script on current file in context
+		vscode.window.activeTerminal?.sendText(`${script} ${currentPath}`);
 	});
 
-	context.subscriptions.push(disposable);
-}
+	let runrunexplorer = vscode.commands.registerCommand(
+		'run-something.runrunexplorer',
+		(uri: vscode.Uri) => {
+			const script = vscode.workspace
+				.getConfiguration('run-something')
+				.get('script') as string;
 
-// This method is called when your extension is deactivated
+			// run script on current folder from context
+			vscode.window.activeTerminal?.sendText(`${script} ${uri.fsPath}`);
+		}
+	);
+
+	context.subscriptions.push(runrun, runrunexplorer);
+}
 export function deactivate() {}
